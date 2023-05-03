9a85aadbff76a0364c33aa2e71aef00b46186e5a
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-05-03 22:54:33 +0530

    client and server initial dependencies

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`MongoDB Connected!`);
+  });
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on ${process.env.PORT}`);
+});
